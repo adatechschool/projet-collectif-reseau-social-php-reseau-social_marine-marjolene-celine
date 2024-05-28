3546dd0c0f5bca2ed3045d809695d43b0fdd4052
Céline <celinesou77@gmail.com>
2024-05-28 12:20:19 +0200

    page tags + fonction displayPost

--- resoc_n1/functions.php
+++ resoc_n1/functions.php
@@ -1,4 +1,40 @@
 <?php
-function connectToDB() {
+function connectToDB()
+{
     return new mysqli("localhost", "root", "", "socialnetwork");
 }
+
+function displayPost($post)
+{
+?>
+    <article>
+        <h3>
+            <time datetime=<?php $post["created"] ?>>
+                <?php
+                setlocale(LC_TIME, "fr_FR.UTF-8", 'fra');
+                echo strftime("%d %B %Y à %Hh%M", strtotime($post['created']));
+                ?>
+            </time>
+        </h3>
+        <address>par <?php echo $post["author_name"] ?></address>
+        <div>
+            <p>
+                <?php
+                echo $post["content"];
+                ?>
+            </p>
+        </div>
+        <footer>
+            <small>♥ <?php echo $post['like_number'] ?> </small>
+            <?php
+            $tags_array = explode(",", $post['taglist']);
+            foreach ($tags_array as $tag) {
+            ?>
+                <a><?php echo "#" . $tag . "," ?></a>
+
+            <?php
+            }
+            ?>
+        </footer>
+    </article>
+<?php } ?>
