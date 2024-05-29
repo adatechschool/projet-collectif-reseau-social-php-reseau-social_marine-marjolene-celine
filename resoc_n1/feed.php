0ebe0c70406385bb34515d82dd9ff1737d6bd4f7
Marine Suhard <marine.suhard@gmail.com>
2024-05-29 18:55:44 +0200

    clean des print-r

--- resoc_n1/feed.php
+++ resoc_n1/feed.php
@@ -1,78 +1,79 @@
 <!doctype html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>ReSoC - Flux</title>         
-        <meta name="author" content="Julien Falconnet">
-        <link rel="stylesheet" href="style.css"/>
-    </head>
-    <body>
-        <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-                </ul>
 
-            </nav>
-        </header>
-        <div id="wrapper">
+<head>
+    <meta charset="utf-8">
+    <title>ReSoC - Flux</title>
+    <meta name="author" content="Julien Falconnet">
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <header>
+        <img src="resoc.jpg" alt="Logo de notre réseau social" />
+        <nav id="menu">
+            <a href="news.php">Actualités</a>
+            <a href="wall.php?user_id=5">Mur</a>
+            <a href="feed.php?user_id=5">Flux</a>
+            <a href="tags.php?tag_id=1">Mots-clés</a>
+        </nav>
+        <nav id="user">
+            <a href="#">Profil</a>
+            <ul>
+                <li><a href="settings.php?user_id=5">Paramètres</a></li>
+                <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
+                <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+            </ul>
+
+        </nav>
+    </header>
+    <div id="wrapper">
+        <?php
+        /**
+         * Cette page est TRES similaire à wall.php. 
+         * Vous avez sensiblement à y faire la meme chose.
+         * Il y a un seul point qui change c'est la requete sql.
+         */
+        /**
+         * Etape 1: Le mur concerne un utilisateur en particulier
+         */
+        $userId = intval($_GET['user_id']);
+        ?>
+        <?php
+        /**
+         * Etape 2: se connecter à la base de donnée
+         */
+
+        include "./functions.php";
+        $mysqli = connectToDB();
+        ?>
+
+        <aside>
             <?php
             /**
-             * Cette page est TRES similaire à wall.php. 
-             * Vous avez sensiblement à y faire la meme chose.
-             * Il y a un seul point qui change c'est la requete sql.
+             * Etape 3: récupérer le nom de l'utilisateur
              */
-            /**
-             * Etape 1: Le mur concerne un utilisateur en particulier
-             */
-            $userId = intval($_GET['user_id']);
+            $laQuestionEnSql = "SELECT * FROM `users` WHERE id= '$userId' ";
+            $lesInformations = $mysqli->query($laQuestionEnSql);
+            $user = $lesInformations->fetch_assoc();
+            //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
             ?>
+            <img src="user.jpg" alt="Portrait de l'utilisatrice" />
+            <section>
+                <h3>Présentation</h3>
+                <p>Sur cette page vous trouverez tous les message des utilisatrices
+                    auxquel est abonnée l'utilisatrice <?php echo $user['alias'] ?>
+                    (n° <?php echo $userId ?>)
+                </p>
+
+            </section>
+        </aside>
+        <main>
             <?php
             /**
-             * Etape 2: se connecter à la base de donnée
+             * Etape 3: récupérer tous les messages des abonnements
              */
-           
-             include "./functions.php";
-             $mysqli = connectToDB();
-            ?>
-
-            <aside>
-                <?php
-                /**
-                 * Etape 3: récupérer le nom de l'utilisateur
-                 */
-                $laQuestionEnSql = "SELECT * FROM `users` WHERE id= '$userId' ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                $user = $lesInformations->fetch_assoc();
-                //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
-                echo "<pre>" . print_r($user, 1) . "</pre>";
-                ?>
-                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
-                <section>
-                    <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez tous les message des utilisatrices
-                        auxquel est abonnée l'utilisatrice XXX
-                        (n° <?php echo $userId ?>)
-                    </p>
-
-                </section>
-            </aside>
-            <main>
-                <?php
-                /**
-                 * Etape 3: récupérer tous les messages des abonnements
-                 */
-                $laQuestionEnSql = "
+            $laQuestionEnSql = "
                     SELECT posts.content,
                     posts.created,
                     users.alias as author_name,  
@@ -88,25 +89,23 @@
                     GROUP BY posts.id
                     ORDER BY posts.created DESC  
                     ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                if ( ! $lesInformations)
-                {
-                    echo("Échec de la requete : " . $mysqli->error);
-                }
+            $lesInformations = $mysqli->query($laQuestionEnSql);
+            if (!$lesInformations) {
+                echo ("Échec de la requete : " . $mysqli->error);
+            }
 
-                /**
-                 * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
-                 * A vous de retrouver comment faire la boucle while de parcours...
-                 */
-                ?> 
-                <?php while ($post = $lesInformations->fetch_assoc()) {
-                    echo "<pre>" . print_r($post, 1) . "</pre>";
+            /**
+             * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
+             * A vous de retrouver comment faire la boucle while de parcours...
+             */
+            ?>
+            <?php while ($post = $lesInformations->fetch_assoc()) {
+                displayPost($post);
+            };
+            ?>
 
-                     displayPost($post);
-                    };
-                    ?>
-                
-            </main>
-        </div>
-    </body>
+        </main>
+    </div>
+</body>
+
 </html>
