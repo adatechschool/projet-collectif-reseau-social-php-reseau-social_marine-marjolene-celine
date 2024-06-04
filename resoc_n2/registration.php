26cf49d278cf177a25b4cd533c8dd470321f1c70
Céline <celinesou77@gmail.com>
2024-06-04 11:04:40 +0200

    correction header des pages niveau 2

--- resoc_n2/registration.php
+++ resoc_n2/registration.php
@@ -1,3 +1,7 @@
+<?php
+include "../resoc_n1/functions.php";
+?>
+
 <!doctype html>
 <html lang="fr">
 
@@ -9,24 +13,9 @@
 </head>
 
 <body>
-    <header>
-        <img src="resoc.jpg" alt="Logo de notre réseau social" />
-        <nav id="menu">
-            <a href="/resoc_n1/news.php">Actualités</a>
-            <a href="/resoc_n1/wall.php?user_id=5">Mur</a>
-            <a href="/resoc_n1/feed.php?user_id=5">Flux</a>
-            <a href="/resoc_n1/tags.php?tag_id=1">Mots-clés</a>
-        </nav>
-        <nav id="user">
-            <a href="#">Profil</a>
-            <ul>
-                <li><a href="/resoc_n1/settings.php?user_id=5">Paramètres</a></li>
-                <li><a href="/resoc_n1/followers.php?user_id=5">Mes suiveurs</a></li>
-                <li><a href="/resoc_n1/subscriptions.php?user_id=5">Mes abonnements</a></li>
-            </ul>
-
-        </nav>
-    </header>
+    <?php
+    display_header();
+    ?>
 
     <div id="wrapper">
 
